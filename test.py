a4b1e65cfcb64026b68d7396e69c526502c0a369
keshav <apple@Apples-MacBook-Pro.local>
2024-06-25 01:58:41 +0530

    changed prompt

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 from analyzable_stock import AnalyzableStock
 from multithreading import generate_all_charts_for_stock
-
+import prompt
+import ai_analysis
 #Uncomment below line to analyze a stock
 stock = AnalyzableStock("ZOMATO.NS")
-stock.analyse()
-
-
+ai_analysis.imageanalysis(stock)
+# stock.analyse()
+print(prompt.PrompText.INITIAL_IMAGE_ANALYSIS.value)
 #Uncomment below line to check chart creation
 # generate_all_charts_for_stock("ZOMATO.NS")
